feb10aa9933b636421cf4a7348c9ad9de9f61f66
T0wh1dul <tawheedul578@gmail.com>
2025-03-01 10:05:05 +0600

    Format building

---
+++ js/bgColor.js
@@ -0,0 +1,14 @@
+const button = document.getElementById('changeColorButton');
+
+button.addEventListener('click', function () {
+    const randomColor = getRandomColor();
+    document.body.style.backgroundColor = randomColor;
+});
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
---
+++ js/nextpage.js
@@ -0,0 +1,4 @@
+const next = document.getElementById("next-page");
+next.addEventListener('click', function () {
+    window.location.href ='nextpage.html'
+})
